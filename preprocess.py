6c0ec831143adbc98c8537188b049adc361b1162
Boris <amgboris@ya.ru>
2025-08-03 19:01:23 +0200

    filter and more graphs

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,19 @@
 import sys
 import mne
+import matplotlib.pyplot as plt  # Import matplotlib for plotting control
+
 
 def main(path: str):
     raw = mne.io.read_raw_edf(path, preload=True)
     raw.set_eeg_reference('average', projection=True)
-    raw.plot(n_channels=64, scalings='auto', duration=5, block=True)
+    n_channels = len(raw.ch_names)
+    print(n_channels)
+    raw.plot(n_channels=n_channels, scalings='auto', duration=5, block=True)
+    raw.filter(8., 30., fir_design='firwin')
+    raw.plot(n_channels=n_channels, scalings='auto', duration=5, block=True)
+    raw.compute_psd(fmin=1, fmax=50).plot()
+    plt.show()
+
 
 
 
